7e2b072637ca6466ca4fe4a2c9db1ed6cd65c9cd
emanuelosva <emanuelosva@gmail.com>
2020-10-20 17:20:40 -0500

    <FIX> migration scripts

--- app/prestart.sh
+++ app/prestart.sh
@@ -1,5 +1,11 @@
 #! /usr/bin/env bash
 
+
+# Init db if not exists migrations
+
+aerich init -t db.settings.TORTOISE_ORM_CONFIG --location db/migrations
+aerich init-db
+
 # Run migrations
 
 aerich migrate
