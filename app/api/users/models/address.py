0a18cd15920be358ab78670dc44d6d13913e7728
emanuelosva <emanuelosva@gmail.com>
2020-10-20 23:53:24 -0500

    <FEATURE> users models.

---
+++ app/api/users/models/address.py
@@ -0,0 +1,40 @@
+"""
+DB - Address model.
+"""
+
+# Base model
+from api.utils.models import MosyneModel
+
+# Tortoise ORM
+from tortoise import fields
+
+
+class Address(MosyneModel):
+    """Address model.
+
+    Make reference to any user (guardian or angel) address.
+    """
+
+    city = fields.CharField(max_length=54)
+
+    municipality = fields.CharField(
+        max_length=54,
+        description="User municipality ref to: `Municipio/delegación`",
+    )
+
+    neighborhood = fields.CharField(
+        max_length=54,
+        description="User neighborhood ref to: `Colonia`",
+    )
+
+    street = fields.CharField(max_length=54)
+    num_int = fields.CharField(max_length=5)
+    num_ext = fields.CharField(max_length=5)
+
+    cp = fields.CharField(max_length=8, description="Postal code")
+
+    class Meta(MosyneModel.Meta):
+        """Meta info."""
+
+        table = "address"
+        abstract = False
