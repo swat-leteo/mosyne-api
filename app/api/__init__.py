770c0eb69e3dd368ad97663feb3d000547206b0e
emanuelosva <emanuelosva@gmail.com>
2020-10-21 18:51:40 -0500

    <FIX> format.

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,8 +2,8 @@
 Main API - Router
 """
 
-from fastapi import APIRouter
 from api.users.router import router as user_router
+from fastapi import APIRouter
 
 ################
 # API - ROUTER #
@@ -13,7 +13,6 @@ api_router = APIRouter()
 
 # --- AUTH router --- #
 # api_router.include_router(auth_router, prefix="/auth", tags=["Auth"])
-
 
 # --- AUTH router --- #
 api_router.include_router(user_router, prefix="/user", tags=["User"])
